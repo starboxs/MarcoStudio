2662b38c1c578d412a04d85f88c42b868d8f7b3a
starboxs <starboxs8917@gmail.com>
2025-03-20 04:27:05 +0800

    123

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -90,10 +90,10 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <h4 className="text-lg font-semibold mb-4">關注我</h4>
+              <h4 className="text-lg font-semibold mb-4">關於我</h4>
               <div className="flex space-x-4">
                 <a
-                  href="#"
+                  href="https://www.facebook.com/marcoliu9527/"
                   className="text-gray-400 hover:text-white transition"
                 >
                   <svg
@@ -110,7 +110,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 <a
-                  href="#"
+                  href="https://www.instagram.com/marco_liu_taiwan/"
                   className="text-gray-400 hover:text-white transition"
                 >
                   <svg
@@ -127,20 +127,7 @@ const Footer = () => {
                   </svg>
                 </a>
                 <a
-                  href="#"
-                  className="text-gray-400 hover:text-white transition"
-                >
-                  <svg
-                    className="w-6 h-6"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
-                  </svg>
-                </a>
-                <a
-                  href="#"
+                  href="https://github.com/starboxs/"
                   className="text-gray-400 hover:text-white transition"
                 >
                   <svg
